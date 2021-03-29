78ac8146569bd4e6923336177de8c81ebbcd1955
ziyawinn <ziyawinn@gmail.com>
2021-03-28 22:51:22 -0400

    mood

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Zwinn99:Winnstorm@cluster1.6xzhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'dbName': 'moodDB'
+};
